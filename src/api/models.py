e10ce1c4b1a6e81b7931cee5380f62fa213c4547
Ben <113471794+CabraCrimes@users.noreply.github.com>
2023-05-07 18:44:25 +0000

    FAVOURITES

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,76 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # favourite = db.relationship("Favourites", backref="user")
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    status = db.Column(db.Boolean, unique=False, nullable=False)
+    species = db.Column(db.String(80), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+            "species": self.species,
+        }
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    type = db.Column(db.String(120), unique=False, nullable=False)
+    dimension = db.Column(db.String(120), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "dimension": self.dimension,
+        }
+
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    air_date = db.Column(db.String(80), unique=False, nullable=False)
+
+
+class Favourites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship(User)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"))
+    people = db.relationship(People)
+    location_id = db.Column(db.Integer, db.ForeignKey("location.id"))
+    location = db.relationship(Location)
+    episode_id = db.Column(db.Integer, db.ForeignKey("episode.id"))
+    episode = db.relationship(Episode)
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "location_id": self.location_id,
+            "episode_id": self.episode_id,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,186 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, People, Location, Favourites
 from api.utils import generate_sitemap, APIException
 
-api = Blueprint('api', __name__)
+api = Blueprint("api", __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route("/hello", methods=["POST", "GET"])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
+
+# GET ALL PEOPLE
+@api.route("/people", methods=["GET"])
+def get_all_people():
+    people = People.query.all()
+    people_serialized = [people.serialize() for people in people]
+    return jsonify({"people": people_serialized}), 200
+
+
+# GET ONE PERSON
+@api.route("/person/<int:person_id>", methods=["GET"])
+def get_one_person(person_id):
+    person = People.query.get(person_id)
+    if not person:
+        return jsonify({"error": "No person with this id found"}), 400
+    return jsonify({"person": person.serialize()}), 200
+
+
+# GET ALL LOCATIONS
+@api.route("/location", methods=["GET"])
+def get_all_location():
+    location = Location.query.all()
+    location_serialized = [location.serialize() for location in location]
+    return jsonify({"location": location_serialized}), 200
+
+
+# GET ONE LOCATION
+@api.route("/location/<int:location_id>", methods=["GET"])
+def get_one_location(location_id):
+    location = Location.query.get(location_id)
+    if not location:
+        return jsonify({"error": "No location with this id found"}), 400
+    return jsonify({"person": location.serialize()}), 200
+
+
+# POST CREATE A PERSON
+@api.route("/people", methods=["POST"])
+def create_person():
+    body = request.json
+    new_person = People(
+        name=body["name"],
+        status=body["status"],
+        species=body["species"],
+    )
+    db.session.add(new_person)
+    db.session.commit()
+    return jsonify({"people": "create"}), 200
+
+
+# POST favourites people
+# use filter_by to find correct user in the favourites table!!
+@api.route("/favorites/people/<int:people_id>", methods=["POST"])
+def add_people_favourites(people_id):
+    favourite = Favourites(
+        user_id=request.json["user_id"], people_id=request.json["people_id"]
+    )
+    db.session.add(favourite)
+    db.session.commit()
+    return jsonify(
+        {"favourite": f"Person {people_id} added to favourites {favourite.user_id}"}
+    )
+
+
+# CREATE/POST FAVOURITE LOCATION
+# use filter_by to find correct user in the favourites table!!
+@api.route("/favorites/location/<int:location_id>", methods=["POST"])
+def add_location_favourites(location_id):
+    # favourite = Favourites(
+    #     user_id=request.json["user_id"], location_id=request.json["location_id"]
+    # )
+    favourite = Favourites(location_id=location_id, user_id=1)
+    db.session.add(favourite)
+    db.session.commit()
+    return jsonify(
+        {
+            "favourite location": f"Location id {location_id} added to favourites {location_id}"
+        }  # return all favourites
+    )
+
+
+######EXAMPLE######
+# POST ONE FAVOURITE PEOPLE
+# @api.route("/favorite/people/<int:people_id> ", methods=["POST"])
+# def add_location_favourites(people_id):
+#     people = People(people_id=people_id, user_id=1)
+#     db.session.add(people)
+#     db.session.commit()
+#     return jsonify(
+#         {
+#             "favourite person": f"Person id {people_id} added to favourites {People.all()}"
+#         }
+#     )
+
+
+# GET ALL USER FAVOURITE LOCATIONS
+# use filter_by to find correct user in the favourites table!!
+@api.route("/user/1/favorites/location/", methods=["GET"])
+def get_all_favourite_locations():
+    location = Location.query.all()
+    location_serialized = [location.serialize() for location in location]
+    return jsonify({"Favourite location": location_serialized}), 200
+
+
+# GET ALL USER FAVOURIRES
+# use filter_by to find correct user in the favourites table!!
+@api.route("/user/favourites", methods=["GET"])
+def get_user_favourites():
+    user_favourites = User.query.all()
+    # use filter_by to find correct user in the favourites table!!
+    user_favourites_serialized = [
+        user_favourites.serialize() for fav in user_favourites
+    ]
+    if not user_favourites:
+        return jsonify({"error": "No favourites found"}), 400
+    return jsonify({"favourite": user_favourites.serialize()}), 200
+
+
+# GET ALL LOCATIONS IN FAVOURITE
+# use filter_by to find correct user in the favourites table!!
+@api.route("favorites/location", methods=["GET"])
+def get_location_favourites():
+    location_favourites = Favourites.query.all()
+    # use filter_by to find correct user
+    user_favourites_serialized = [
+        location_favourites.serialize() for fav in location_favourites
+    ]
+    if not location_favourites:
+        return jsonify({"error": "No favourites found"}), 400
+    return jsonify({"favourite": location_favourites.serialize()}), 200
+
+
+# GET ONE USER
+@api.route("/user/<int:user_id>", methods=["GET"])
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "No user with this id found"}), 400
+    return jsonify({"person": user.serialize()}), 200
+
+
+# GET ALL USER
+@api.route("/user", methods=["GET"])
+def get_all_users():
+    user = User.query.all()
+    user_serialized = [user.serialize() for user in user]
+    return jsonify({"people": user_serialized}), 200
+
+
+# DELETE FAVOURITE PEOPLE
+# use filter_by to find correct user in the favourites table!!
+@api.route("/favorites/people/<int:people_id>", methods=["DELETE"])
+def delete_one_people(people_id):
+    people = People.query.get(people_id)
+    if not people:
+        return jsonify({"error": "No people with this id found"}), 400
+    db.session.delete(people)
+    db.session.commit()
+    return jsonify({"person": "people deleted"}), 200
+
+
+# DELETE FAVOURITE LOCATION
+# use filter_by to find correct user in the favourites table!!
+@api.route("/favorites/location/<int:location_id>", methods=["DELETE"])
+def delete_one_location(location_id):
+    location = Location.query.get(location_id)
+    if not location:
+        return jsonify({"error": "No location with this id found"}), 400
+    db.session.delete(location)
+    db.session.commit()
+    return jsonify({"location": "location deleted"}), 200
